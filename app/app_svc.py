849859a2072f9729f24ca773da719ff22662fdb4
Scott Cronin <scronin@trunkclub.com>
2018-04-02 09:13:34 -0500

    flask app up and running

--- app/app_svc.py
+++ app/app_svc.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, jsonify, abort
+import json
+import numpy as np
+
+with open('app/objects/user_profiles.json', 'r') as f:
+    USER_PROFILES = json.load(f)
 
 app = Flask(__name__)
 
@@ -6,12 +11,19 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
+@app.route('/recommender')
+def recommender():
+    sample_user_size = 30
+    users = np.random.choice(list(USER_PROFILES), size=30, replace=False)
+    profiles = [(int(user), USER_PROFILES[user]) for user in users]
+    return render_template('recommender.html', value=profiles)
+
 @app.route('/rec_predict', methods=['POST'])
 def rec_predict():
     payload = request.get_json()
 
     # recs = MODEL.predict(payload)
-    recs = ['movie_1', 'movie2', 'movie3']
+    recs = ['movie_1', 'movie_2', 'movie_3']
     output = {
         'user_id': 0,
         'recs': recs,
